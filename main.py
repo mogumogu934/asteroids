5fad1001c186a14e00e821d36691f4c23654a234
Emphy934 <emphs17@gmail.com>
2025-01-11 02:08:55 -0800

    Create a player ship

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +10,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
- 
+            
+        dt = clock.tick(60) / 1000
+        for object in updatable:
+            object.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
 
